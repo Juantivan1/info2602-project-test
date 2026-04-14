2d478996ee7e880ec0663fa8792c06f3fd9f6613
Juantivan1 <166667329+Juantivan1@users.noreply.github.com>
2026-04-14 06:52:32 -0400

    model fix

--- app/models/workout_routine.py
+++ app/models/workout_routine.py
@@ -7,13 +7,14 @@ if TYPE_CHECKING:
 
 
 class RoutineWorkoutBase(SQLModel):
-    routine_id: Optional[int] = Field(default=None, foreign_key="routine.id", primary_key=True)
-    workout_id: Optional[int] = Field(default=None, foreign_key="workout.id", primary_key=True)
+    routine_id: Optional[int] = Field(default=None, foreign_key="routine.id")
+    workout_id: Optional[int] = Field(default=None, foreign_key="workout.id")
     order: int = 0
     sets: int = 3
     reps: int = 10
 
 
 class RoutineWorkout(RoutineWorkoutBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     routine: Optional["Routine"] = Relationship(back_populates="workout_links")
     workout: Optional["Workout"] = Relationship(back_populates="routine_links")
